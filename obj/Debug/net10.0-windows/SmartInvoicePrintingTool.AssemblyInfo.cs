d44e45b25cf9e1f6268cff84d42e077e98a83533
Eureka <scorpiowzf@gmail.com>
2026-07-23 19:19:08 +0800

    更新程序集信息版本并同步生成的文件列表

--- obj/Debug/net10.0-windows/SmartInvoicePrintingTool.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/SmartInvoicePrintingTool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartInvoicePrintingTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c7ff2ddf8adc5f06fb281264dee6230f5b5d5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f0c8cdba562cb30a5b19e1a99a8f47aeaacb49f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartInvoicePrintingTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartInvoicePrintingTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
